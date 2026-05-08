5d6e20c69fc5bf7e447a2c8537a4939ac2f0263e
Dani Raulisa <graulisa@gmail.com>
2026-05-08 11:38:39 +0000

    Nudge the pulsing indicator a little to the left

--- src/components/ChatbotThemba.tsx
+++ src/components/ChatbotThemba.tsx
@@ -208,7 +208,7 @@ export function ChatbotThemba() {
         
         {/* Online Status Indicator */}
         {!isOpen && (
-          <span className="absolute -top-1 -right-1 flex h-4 w-4">
+          <span className="absolute -top-1 right-1 flex h-4 w-4">
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
             <span className="relative inline-flex rounded-full h-4 w-4 bg-green-500 border-2 border-[#020c1b]"></span>
           </span>
